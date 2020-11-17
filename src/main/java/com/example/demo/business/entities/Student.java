d25278327dfe871496e31e456841687584185ddc
kamilnaja <knaja@pentacomp.pl>
2020-11-17 10:32:51 +0100

    adding JPA

---
+++ src/main/java/com/example/demo/business/entities/Student.java
@@ -0,0 +1,7 @@
+package com.example.demo.business.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student {
+}
--- src/main/java/com/example/demo/web/controller/HomeController.java
+++ src/main/java/com/example/demo/web/controller/HomeController.java
@@ -1,20 +1,12 @@
 package com.example.demo.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.thymeleaf.ITemplateEngine;
-import org.thymeleaf.context.WebContext;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Calendar;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController implements IGTVController {
-    @Override
-    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws Exception {
-        WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-        ctx.setVariable("today", Calendar.getInstance());
-        templateEngine.process("home", ctx, response.getWriter());
+public class HomeController {
+    @RequestMapping("/home")
+    public String test() {
+        return "index";
     }
 }
